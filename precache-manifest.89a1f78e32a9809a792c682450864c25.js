9c0ca8f4be4406a95ea0d0979548f793c5e6de11
emmattfu <makcimdjan@gmail.com>
2020-06-17 13:33:09 +0300

    Updates

---
+++ precache-manifest.89a1f78e32a9809a792c682450864c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9ab0369c703db31ccef59aa9ba21f5",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "797a0c5f4f93de86ec06",
+    "url": "/recipes/static/css/main.fd7271d1.chunk.css"
+  },
+  {
+    "revision": "dc4c95629b867a0abd9f",
+    "url": "/recipes/static/js/2.88d2c109.chunk.js"
+  },
+  {
+    "revision": "6def8d787cc4234a4738775789db8a47",
+    "url": "/recipes/static/js/2.88d2c109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797a0c5f4f93de86ec06",
+    "url": "/recipes/static/js/main.1e4046a4.chunk.js"
+  },
+  {
+    "revision": "189c71edee46f3c07be4",
+    "url": "/recipes/static/js/runtime-main.baae4267.js"
+  },
+  {
+    "revision": "60f70df5bb3ca9d7fcd68d628e2e628c",
+    "url": "/recipes/static/media/home.60f70df5.jpg"
+  }
+]);
